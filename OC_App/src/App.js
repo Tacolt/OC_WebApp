b8f8a2e096cd8ca0459475e5915a12265d49f71c
Colt Thompson <tacolt@bgsu.edu>
2017-02-20 19:57:09 -0500

    Removed LineSelection.js and .css. Added the code to ImageSlider.js

--- OC_App/src/App.js
+++ OC_App/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import HelloWorld from './HelloWorld';
-import LineSelection from './LineSelection';
+//import LineSelection from './LineSelection';
 import SimpleSlider from './ImageSlider';
 
 class App extends Component {
@@ -14,16 +14,23 @@ class App extends Component {
   render() {
   	return (
     <div>
-    	<LineSelection/>
     	<br/>
-    	<br/>
-    	<br/>
-    	<br/>
-    	<br/>
-    	<SimpleSlider/>
+    	<SimpleSlider name="https://dcpd6wotaa0mb.cloudfront.net/mdms/uploads/shingle_colors/swatch_768x768s/000/000/136/original/PYS_Berk_CanterburyBlack_768x768_72dpi.jpg?1476293729"/>
     </div>
     );
-  }	
+  }
+
+  handleLineChange(){
+
+  }
+
+  handleTextChange(){
+  	
+  }
+
+  handleImageChange(){
+
+  }
 };
 
 export default App;
--- OC_App/src/ImageSlider.js
+++ OC_App/src/ImageSlider.js
@@ -3,7 +3,7 @@ var Slider = require('react-slick');
 import './ImageSlider.css';
 import './slick.css';
 
-//react-slick -- node_module
+//react-slick -- node_module (JSX)
 var SimpleSlider = React.createClass({
   render: function() {
     var settings = {
@@ -14,27 +14,34 @@ var SimpleSlider = React.createClass({
     }
     return (
       <div>
+        <div className="LineSelection">
+          <select onchange="updateText()" id="soflow">
+            <option>{this.props.name}</option>
+            <option>{this.props.name}</option>
+            <option>{this.props.name}</option>
+          </select>
+        </div>
         <Slider>
-          <div><img className="sliderImage" src='https://dcpd6wotaa0mb.cloudfront.net/mdms/uploads/shingle_colors/swatch_768x768s/000/000/136/original/PYS_Berk_CanterburyBlack_768x768_72dpi.jpg?1476293729'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=2'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=3'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=4'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=5'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=6'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=7'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=8'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=9'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=10'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=11'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=12'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=13'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=14'/></div>
-          <div><img className="sliderImage" src='http://placehold.it/350x300?text=15'/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={this.props.name}/></div>
         </Slider>
 
         <ul>
          <li>
-           <a href='http://placehold.it/350x300?text=1'><img className="imageArray" src='http://placehold.it/350x300?text=1'/></a>
+           <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
            <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
            <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
            <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
@@ -47,6 +54,8 @@ var SimpleSlider = React.createClass({
     );
   }
 });
+
+
 
 
 export default SimpleSlider
