c1e9b9bf960a03b7cd9ab6f59b1e6ba5d6344ddc
Colt Thompson <tacolt@bgsu.edu>
2017-02-21 06:13:14 -0500

    Moved some stuff around. Lots of fiddeling, no major changes.

--- OC_App/src/App.js
+++ OC_App/src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
+import './ImageSlider.css';
 import SimpleSlider from './ImageSlider';
 var $ = require('jquery')
 
 export default class ShingleList extends React.Component {
 
-  //	
+  getInitialState() {
+  	return
+  		value: 'berkshire';
+  }
+   
+  //
   constructor(props) {
     super(props);
     this.state = {shingle: []};
+    this.change = this.change.bind(this);
+    
   }
   componentDidMount() {
     this.ShingleList();
@@ -16,51 +24,49 @@ export default class ShingleList extends React.Component {
 
   //**ShingleList() -- gets API data stores in shingle prop
   ShingleList() {
-    return $.getJSON('https://mdms.owenscorning.com/api/v1/product/shingles?zip=43659')
+    return $.getJSON('https://mdms.owenscorning.com/api/v1/product/shingles?zip=43659') //API call
       .then((data) => {
         this.setState({ shingle: data });
       });
   }
 
+  change(event){
+  	this.setState({value: event.target.value});
+  }
+
   //**RENDER() -- renders elements to screen
   render() {
 
-  	//Maps shingle line name to lineName
+  	//**Map() -- creates new array from shingle array
     const lineName = this.state.shingle.map((item, i) => {
       return <div>
         <h1>{item.name}</h1>
       </div>
     });
 
-    //Maps shingle line name to lineName
-    const colorName = this.state.shingle.map((item, i) => {
-      return <div>
-        <h1>{item.shingle_colors[0].name}</h1>
-      </div>
-    });
+    
 
-    var swatchURL = this.state.shingle.map((item, i) => {
-      return <div>
-        <h1>{item.shingle_colors[0].swatch_image_url}</h1>
-      </div>
-    });
+    
 
     //Displays Line Name + Color Name
     return <div>
       <div>
-      <div>{lineName[0]}{colorName[0]}{swatchURL[0]}</div>
-          <select>
-            <option>{lineName[0]}</option>
-            <option>{lineName[1]}</option>
-            <option>{lineName[2]}</option>
-            <option>{lineName[3]}</option>
-            <option>{lineName[4]}</option>
-            <option>{lineName[5]}</option>
-            <option>{lineName[6]}</option>
-            <option>{lineName[7]}</option>
-          </select>
-        </div>
 
+      	  <div><h1>{this.state.value}</h1></div>
+          <select className="soflow" onChange={this.change} value={this.state.value}>
+            <option value={lineName[0]}>{lineName[0]}</option>
+            <option value='devonshire'>{lineName[1]}</option>
+            <option value='durationSTORM'>{lineName[2]}</option>
+            <option value='durationCOOL'>{lineName[3]}</option>
+            <option value='durationDESIGN'>{lineName[4]}</option>
+            <option value='duration'>{lineName[5]}</option>
+            <option value='oakridge'>{lineName[6]}</option>
+            <option value='supreme'>{lineName[7]}</option>
+          </select>
+
+          
+        </div>
+       <br/>
        <SimpleSlider/>
 
     </div>
--- OC_App/src/ImageSlider.js
+++ OC_App/src/ImageSlider.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 var Slider = require('react-slick');
-var $ = require('jquery')
+var $ = require('jquery');
 import './ImageSlider.css';
 import './slick.css';
 
 //react-slick -- (node_module)
 class SimpleSlider extends React.Component {
 
+  getInitialState() {
+    return
+        value: ' '; 
+  }
+
+
   constructor(props) {
     super(props);
     this.state = {shingle: []};
+    this.nextColor = this.nextColor.bind(this);
+    //this.previousColor = this.previousColor.bind(this);
   }
 
   componentDidMount() {
@@ -23,7 +31,17 @@ class SimpleSlider extends React.Component {
       });
   }
 
+  nextColor(event){
+     this.setState({value: event.target.value});
+  }
+
+  nextColor(event){
+     this.setState({value: event.target.value});
+  }
+
   render() {
+
+    //react-slick -- (node_module) -- Settings: refer to package site for properties
     var settings = {
       dots: false,
       infinite: true,
@@ -31,41 +49,58 @@ class SimpleSlider extends React.Component {
       slidesToScroll: 3,
     }
 
+    //Maps shingle line name to lineName
+    const colorName = this.state.shingle.map((item, i) => {
+      return <div>
+        <h1>{item.shingle_colors[0].name}</h1>
+      </div>
+    });
+
     const swatchURL = this.state.shingle.map((item, i) => {
       return <div>
         <h1>{item.shingle_colors[0].swatch_image_url}</h1>
       </div>
     });
 
+    //react-slick -- (node_module)
     return (
       <div>
+        <h1>{this.state.value}</h1>
         <Slider>
           <div><img className="sliderImage" src={swatchURL[0]}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
-          <div><img className="sliderImage" src={this.props.name}/></div>
+          <div><img className="sliderImage" src={swatchURL[1]}/></div>
+          <div><img className="sliderImage" src={swatchURL[2]}/></div>
+          <div><img className="sliderImage" src={swatchURL[3]}/></div>
+          <div><img className="sliderImage" src={swatchURL[4]}/></div>
+          <div><img className="sliderImage" src={swatchURL[5]}/></div>
+          <div><img className="sliderImage" src={swatchURL[6]}/></div>
+          <div><img className="sliderImage" src={swatchURL[7]}/></div>
+          <div><img className="sliderImage" src={swatchURL[8]}/></div>
+          <div><img className="sliderImage" src={swatchURL[9]}/></div>
+          <div><img className="sliderImage" src={swatchURL[10]}/></div>
+          <div><img className="sliderImage" src={swatchURL[11]}/></div>
+          <div><img className="sliderImage" src={swatchURL[12]}/></div>
+          <div><img className="sliderImage" src={swatchURL[13]}/></div>
+          <div><img className="sliderImage" src={swatchURL[14]}/></div>
         </Slider>
 
         <ul>
          <li>
            <img className="imageArray" src={swatchURL[0]}/>
-           <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
-           <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
-           <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
-           <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
-           <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
-           <img className="imageArray" src='http://placehold.it/350x300?text=1'/>
+           <img className="imageArray" src={swatchURL[1]}/>
+           <img className="imageArray" src={swatchURL[2]}/>
+           <img className="imageArray" src={swatchURL[3]}/>
+           <img className="imageArray" src={swatchURL[4]}/>
+           <img className="imageArray" src={swatchURL[5]}/>
+           <img className="imageArray" src={swatchURL[6]}/>
+           <img className="imageArray" src={swatchURL[7]}/>
+           <img className="imageArray" src={swatchURL[8]}/>
+           <img className="imageArray" src={swatchURL[9]}/>
+           <img className="imageArray" src={swatchURL[10]}/>
+           <img className="imageArray" src={swatchURL[11]}/>
+           <img className="imageArray" src={swatchURL[12]}/>
+           <img className="imageArray" src={swatchURL[13]}/>
+           <img className="imageArray" src={swatchURL[14]}/>
          </li>
         </ul>
       </div>
